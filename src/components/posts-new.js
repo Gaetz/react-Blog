ea8cb6f2c09a6faee3d31cae927984e783be9f65
Gaetz <gaetz@rufflerim.com>
2017-11-07 12:07:01 +0100

    Fields and validation

--- src/components/posts-new.js
+++ src/components/posts-new.js
@@ -1,13 +1,58 @@
 import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
-    render() {
-        return(
-            <div>
-                Post new
+
+    renderField(field) {
+        const { meta: { touched, error } } = field;
+        const className = `form-group ${touched && error ? 'has-danger' : ''}`
+
+        return (
+            <div className={className}>
+                <label>{field.label}</label>
+                <input type='text' className='form-control' {...field.input} />
+                <div className='text-help'>
+                    {touched ? error : ''}
+                </div>
             </div>
+        );
+    }
+
+    onSubmit(values) {
+        console.log(values);
+    }
+
+    render() {
+        const { handleSubmit } = this.props;
+
+        return (
+            <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
+                <Field name='title' component={this.renderField} label='Title' />
+                <Field name='tags' component={this.renderField} label='Categories' />
+                <Field name='content' component={this.renderField} label='Post Content' />
+                <button type='submit' className='btn btn-primary'>Submit</button>
+            </form>
         );
     }
 }
 
-export default PostsNew;
+function validate(values) {
+    const errors = {};
+
+    if (!values.title) {
+        errors.title = 'Title is mandatory';
+    }
+    if (!values.tags) {
+        errors.tags = 'Enter at least a category';
+    }
+    if (!values.content || values.content.length < 3) {
+        errors.content = 'Content is mandatory, and must be at least 3 characters.';
+    }
+
+    return errors;
+}
+
+export default reduxForm({
+    validate,
+    form: 'PostsNewForm'
+})(PostsNew);
